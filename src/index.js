4125f707b36db646bebe2c3287a0fc703705e19c
matosr96 <19matos96@gmail.com>
2023-07-12 16:06:53 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import paymentsRoutes from "./routes/payment.js";
 
 const app = express();
 
+app.use(paymentsRoutes);
 app.listen(3000);
 
 console.log("Server on port", 3000);
